ceb7b369c663e091dde0d24f12123a94e989e933
Eva Neff <evaneff@gmail.com>
2023-01-26 14:22:16 -0700

    maybe this time

--- src/com/amazon/ata/datastore/PackagingDatastore.java
+++ src/com/amazon/ata/datastore/PackagingDatastore.java
@@ -14,33 +14,45 @@ public class PackagingDatastore {
      * The stored pairs of fulfillment centers to the packaging options they support.
      */
     private final List<FcPackagingOption> fcPackagingOptions = Arrays.asList(
-            createFcPackagingOption("IND1", Material.CORRUGATE, "10", "10", "10"),
-            createFcPackagingOption("ABE2", Material.CORRUGATE, "20", "20", "20"),
-            createFcPackagingOption("ABE2", Material.CORRUGATE, "40", "40", "40"),
-            createFcPackagingOption("YOW4", Material.CORRUGATE, "10", "10", "10"),
-            createFcPackagingOption("YOW4", Material.CORRUGATE, "20", "20", "20"),
-            createFcPackagingOption("YOW4", Material.CORRUGATE, "60", "60", "60"),
-            createFcPackagingOption("IAD2", Material.CORRUGATE, "20", "20", "20"),
-            createFcPackagingOption("IAD2", Material.CORRUGATE, "20", "20", "20"),
-            createFcPackagingOption("PDX1", Material.CORRUGATE, "40", "40", "40"),
-            createFcPackagingOption("PDX1", Material.CORRUGATE, "60", "60", "60"),
-            createFcPackagingOption("PDX1", Material.CORRUGATE, "60", "60", "60"));
-//            createFcPackagingOption("IAD2", Material.LAMINATED_PLASTIC, "2000"),
-//            createFcPackagingOption("IAD2", Material.LAMINATED_PLASTIC, "10000"),
-//            createFcPackagingOption("IAD2", Material.LAMINATED_PLASTIC, "5000"),
-//            createFcPackagingOption("YOW4", Material.LAMINATED_PLASTIC, "2000"),
-//            createFcPackagingOption("YOW4", Material.LAMINATED_PLASTIC, "5000"),
-//            createFcPackagingOption("YOW4", Material.LAMINATED_PLASTIC, "10000"),
-//            createFcPackagingOption("IND1", Material.LAMINATED_PLASTIC, "2000"),
-//            createFcPackagingOption("IND1", Material.LAMINATED_PLASTIC, "5000"),
-//            createFcPackagingOption("ABE2", Material.LAMINATED_PLASTIC, "2000"),
-//            createFcPackagingOption("ABE2", Material.LAMINATED_PLASTIC, "6000"),
-//            createFcPackagingOption("PDX1", Material.LAMINATED_PLASTIC, "5000"),
-//            createFcPackagingOption("PDX1", Material.LAMINATED_PLASTIC, "10000"),
-//            createFcPackagingOption("YOW4", Material.LAMINATED_PLASTIC, "5000")
+            createFcPackagingOption("IND1", "10", "10", "10"),
+            createFcPackagingOption("ABE2", "20", "20", "20"),
+            createFcPackagingOption("ABE2", "40", "40", "40"),
+            createFcPackagingOption("YOW4", "10", "10", "10"),
+            createFcPackagingOption("YOW4", "20", "20", "20"),
+            createFcPackagingOption("YOW4",  "60", "60", "60"),
+            createFcPackagingOption("IAD2",  "20", "20", "20"),
+            createFcPackagingOption("IAD2",  "20", "20", "20"),
+            createFcPackagingOption("PDX1",  "40", "40", "40"),
+            createFcPackagingOption("PDX1",  "60", "60", "60"),
+            createFcPackagingOption("PDX1",  "60", "60", "60")
+//            createFcPackagingOption("IAD2", "5000"),
+//            createFcPackagingOption("IAD2",  "10000"),
+//            createFcPackagingOption("IAD2",  "5000"),
+//            createFcPackagingOption("YOW4",  "2000"),
+//            createFcPackagingOption("YOW4",  "5000"),
+//            createFcPackagingOption("YOW4",  "10000"),
+//            createFcPackagingOption("IND1",  "2000"),
+//            createFcPackagingOption("IND1",  "5000"),
+//            createFcPackagingOption("ABE2",  "2000"),
+//            createFcPackagingOption("ABE2",  "6000"),
+//            createFcPackagingOption("PDX1",  "5000"),
+//            createFcPackagingOption("PDX1",  "10000"),
+//            createFcPackagingOption("YOW4",  "5000")
+            );
+    FcPackagingOption newOption1 = createFcPackagingOption("IAD2", "5000");
+    FcPackagingOption newOption2 = createFcPackagingOption("YOW4", "2000");
+    FcPackagingOption newOption3 = createFcPackagingOption("YOW4", "5000");
+    FcPackagingOption newOption4 = createFcPackagingOption("YOW4", "10000");
+    FcPackagingOption newOption5 = createFcPackagingOption("IND1", "2000");
+    FcPackagingOption newOption6 = createFcPackagingOption("IND1", "5000");
+    FcPackagingOption newOption7 = createFcPackagingOption("ABE2", "2000");
+    FcPackagingOption newOption8 = createFcPackagingOption("ABE2", "6000");
+    FcPackagingOption newOption9 = createFcPackagingOption("PDX1", "5000");
+    FcPackagingOption newOption10 = createFcPackagingOption("PDX1", "10000");
+    FcPackagingOption newOption11 = createFcPackagingOption("YOW4", "5000");
 
 
-    private FcPackagingOption createFcPackagingOption(String fcCode, Material material, String volume) {
+    private FcPackagingOption createFcPackagingOption(String fcCode, String volume) {
         FulfillmentCenter fulfillmentCenter = new FulfillmentCenter(fcCode);
         Packaging packaging = new PolyBag(new BigDecimal(volume));
         return new FcPackagingOption(fulfillmentCenter, packaging);
@@ -49,7 +61,7 @@ public class PackagingDatastore {
     /**
      * Create fulfillment center packaging option from provided parameters.
      */
-    private FcPackagingOption createFcPackagingOption(String fcCode, Material material,
+    private FcPackagingOption createFcPackagingOption(String fcCode,
                                                       String length, String width, String height) {
         FulfillmentCenter fulfillmentCenter = new FulfillmentCenter(fcCode);
         Packaging packaging = new Box(new BigDecimal(length), new BigDecimal(width),
