2abd5e714a4530a9a089b809473c1af0ce7064fa
Eva Neff <evaneff@gmail.com>
2023-01-26 14:27:44 -0700

    fixed checkstyle

--- src/com/amazon/ata/datastore/PackagingDatastore.java
+++ src/com/amazon/ata/datastore/PackagingDatastore.java
@@ -9,6 +9,18 @@ import java.util.*;
  * Stores all configured packaging pairs for all fulfillment centers.
  */
 public class PackagingDatastore {
+    
+    FcPackagingOption newOption1 = createFcPackagingOption("IAD2", "5000");
+    FcPackagingOption newOption2 = createFcPackagingOption("YOW4", "2000");
+    FcPackagingOption newOption3 = createFcPackagingOption("YOW4", "5000");
+    FcPackagingOption newOption4 = createFcPackagingOption("YOW4", "10000");
+    FcPackagingOption newOption5 = createFcPackagingOption("IND1", "2000");
+    FcPackagingOption newOption6 = createFcPackagingOption("IND1", "5000");
+    FcPackagingOption newOption7 = createFcPackagingOption("ABE2", "2000");
+    FcPackagingOption newOption8 = createFcPackagingOption("ABE2", "6000");
+    FcPackagingOption newOption9 = createFcPackagingOption("PDX1", "5000");
+    FcPackagingOption newOption10 = createFcPackagingOption("PDX1", "10000");
+    FcPackagingOption newOption11 = createFcPackagingOption("YOW4", "5000");
 
     /**
      * The stored pairs of fulfillment centers to the packaging options they support.
@@ -39,17 +51,7 @@ public class PackagingDatastore {
 //            createFcPackagingOption("PDX1",  "10000"),
 //            createFcPackagingOption("YOW4",  "5000")
             );
-    FcPackagingOption newOption1 = createFcPackagingOption("IAD2", "5000");
-    FcPackagingOption newOption2 = createFcPackagingOption("YOW4", "2000");
-    FcPackagingOption newOption3 = createFcPackagingOption("YOW4", "5000");
-    FcPackagingOption newOption4 = createFcPackagingOption("YOW4", "10000");
-    FcPackagingOption newOption5 = createFcPackagingOption("IND1", "2000");
-    FcPackagingOption newOption6 = createFcPackagingOption("IND1", "5000");
-    FcPackagingOption newOption7 = createFcPackagingOption("ABE2", "2000");
-    FcPackagingOption newOption8 = createFcPackagingOption("ABE2", "6000");
-    FcPackagingOption newOption9 = createFcPackagingOption("PDX1", "5000");
-    FcPackagingOption newOption10 = createFcPackagingOption("PDX1", "10000");
-    FcPackagingOption newOption11 = createFcPackagingOption("YOW4", "5000");
+
 
 
     private FcPackagingOption createFcPackagingOption(String fcCode, String volume) {
